93122e4fba8ae5eb0024530ae2512bcf5c52da85
spuro <36612221+spuro@users.noreply.github.com>
2023-08-02 20:44:44 +0930

    apos rdquo

--- src/components/writings/Schizo888.tsx
+++ src/components/writings/Schizo888.tsx
@@ -13,25 +13,26 @@ const Schizo888 = () => {
       <br />
       <p>
         Eyes wide, all scattered like. Picture it, right? An 888 beaming through
-        the skitter-skatter of the mind's eye. Triples, see? Triples in the
+        the skitter-skatter of the mind&apos;s eye. Triples, see? Triples in the
         cosmos, like a kaleidoscope gone mad. Whoo! Eight, infinity stood tall!
-        A loop, a snake eatin' its own tail, ouroboros-like, but straight as an
-        arrow, man! Now triple that! Cosmic giggles, its a ride, a mad, wild
-        ride! See, 888, it's a signal from the ether, man. Telling you you're on
-        the money, like gold, pure, infinite, you know? Glowing like a starburst
-        candy on a summer sidewalk. Beaming! Bouncing off the walls of the
-        cosmos!
+        A loop, a snake eatin&apos; its own tail, ouroboros-like, but straight
+        as an arrow, man! Now triple that! Cosmic giggles, its a ride, a mad,
+        wild ride! See, 888, it&apos;s a signal from the ether, man. Telling you
+        you&apos;re on the money, like gold, pure, infinite, you know? Glowing
+        like a starburst candy on a summer sidewalk. Beaming! Bouncing off the
+        walls of the cosmos!
         <br />
         <br />
-        But hey, hold up! 888, man. It's about the balance too, right? Three
-        spheres in a line, like pearls on a necklace of reality. An infinity
-        mirrored in threes. 888 telling you, "Hold on, Jack! It ain't a free
-        lunch!" You've gotta give to get, gotta balance the scales. Infinite
-        abundance? Sure, but what you puttin' out, man? What you puttin' out
-        into the cosmic jukebox? See, the world's a mirror, all echoing back. Be
-        the change, they say. 888, it's a cosmic tap on the shoulder. Be the
-        echo. Be the light, and let it bounce right back to you, all infinite
-        and golden. Just like 888. Can you dig it, man?
+        But hey, hold up! 888, man. It&apos;s about the balance too, right?
+        Three spheres in a line, like pearls on a necklace of reality. An
+        infinity mirrored in threes. 888 telling you, &rdquo;Hold on, Jack! It
+        ain&apos;t a free lunch!&rdquo; You&apos;ve gotta give to get, gotta
+        balance the scales. Infinite abundance? Sure, but what you puttin&apos;
+        out, man? What you puttin&apos; out into the cosmic jukebox? See, the
+        world&apos;s a mirror, all echoing back. Be the change, they say. 888,
+        it&apos;s a cosmic tap on the shoulder. Be the echo. Be the light, and
+        let it bounce right back to you, all infinite and golden. Just like 888.
+        Can you dig it, man?
       </p>
     </div>
   );
