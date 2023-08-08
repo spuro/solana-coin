8465419ced3dd4d19fb0a3c254d6d98f765d0057
spuro <36612221+spuro@users.noreply.github.com>
2023-08-08 14:12:07 +0930

    remove overlayer

--- src/components/overlayer/ImagePreview.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ImagePreview = ({ file }) => {
-  const url = file ? URL.createObjectURL(file) : null;
-
-  return <div>{url && <img src={url} alt="Preview" />}</div>;
-};
-
-export default ImagePreview;
